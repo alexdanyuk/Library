9168b8a1284ae4c06dedef605d0ef4ce8ca89e7d
alexdanyuk <danyk.alex@gmail.com>
2018-10-24 23:26:09 +0300

    cm

---
+++ CryptographyLib/Protector.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Xml.Linq;
+namespace CryptographyLib
+{
+    public class Protector
+    {
+        private static readonly byte[] salt = Encoding.Unicode.GetBytes("7BANANAS");
+        private static readonly int iterations = 2000;
+        public static string Encrypt(string plainText, string password)
+        {
+            byte[] plainBytes = Encoding.Unicode.GetBytes(plainText);
+            var aes = Aes.Create();
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, iterations);
+            aes.Key = pbkdf2.GetBytes(32);
+            aes.IV = pbkdf2.GetBytes(16);
+            var ms = new MemoryStream();
+            using(var cs = new CryptoStream(ms, aes.CreateDecryptor(), CryptoStreamMode.Write))
+            {
+                cs.Write(plainBytes,0, plainBytes.Length);
+            }
+            return Convert.ToBase64String(ms.ToArray());
+            //dfjhsdf
+        }
+
+        public static string Decrypt(string cryptoText, string password)
+        {
+            byte[] cryptoBytes = Convert.FromBase64String(cryptoText);
+            var aes = Aes.Create();
+            var pbkdf2 = new Rfc2898DeriveBytes(password,salt,iterations);
+            aes.Key = pbkdf2.GetBytes(32);
+            aes.IV = pbkdf2.GetBytes(16);
+            var ms = new MemoryStream();
+            using(var cs = new CryptoStream(ms,aes.CreateDecryptor(),CryptoStreamMode.Write))
+            {
+                cs.Write(cryptoBytes,0,cryptoBytes.Length);
+            }
+            return Encoding.Unicode.GetString(ms.ToArray());
+
+        }
+    }
+}
